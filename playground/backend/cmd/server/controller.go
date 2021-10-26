506372ce3f92d6e66697a523f48bb9f247e53f94
Artur Khanin <artur.khanin@akvelon.com>
2021-10-26 16:35:05 +0300

    Implemented GetRunOutput and covered with unit tests

--- playground/backend/cmd/server/controller.go
+++ playground/backend/cmd/server/controller.go
@@ -15,13 +15,18 @@
 package main
 
 import (
+	"beam.apache.org/playground/backend/internal/cache"
+	"beam.apache.org/playground/backend/internal/errors"
 	"context"
+	"log"
 
 	pb "beam.apache.org/playground/backend/internal/api/v1"
 	"github.com/google/uuid"
 )
 
 type playgroundController struct {
+	cacheService cache.Cache
+
 	pb.UnimplementedPlaygroundServiceServer
 }
 
@@ -42,8 +47,17 @@ func (controller *playgroundController) CheckStatus(ctx context.Context, info *p
 
 //GetRunOutput is returning output of execution for specific pipeline by PipelineUuid
 func (controller *playgroundController) GetRunOutput(ctx context.Context, info *pb.GetRunOutputRequest) (*pb.GetRunOutputResponse, error) {
-	// TODO implement this method
-	pipelineResult := pb.GetRunOutputResponse{Output: "Test Pipeline Result"}
+	pipelineId := info.PipelineUuid
+	runOutputInterface, err := controller.cacheService.GetValue(ctx, uuid.MustParse(pipelineId), cache.RunOutput)
+	if err != nil {
+		log.Printf("%s: GetRunOutput(): cache.GetValue: error: %s", pipelineId, err.Error())
+		return nil, errors.NotFoundError("GetRunOutput", "there is no run output for pipelineId: "+pipelineId+", subKey: cache.SubKey_RunOutput")
+	}
+	runOutput, converted := runOutputInterface.(string)
+	if !converted {
+		return nil, errors.InternalError("GetRunOutput", "run output can't be converted to string")
+	}
+	pipelineResult := pb.GetRunOutputResponse{Output: runOutput}
 
 	return &pipelineResult, nil
 }
